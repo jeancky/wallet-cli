7b2def6d9fef730f73adf07f90e5f437caaef091
Fallace <fxu1990@gmail.com>
2018-12-02 22:18:49 +0800

    上线脚本

---
+++ noah/deploy.sh
@@ -0,0 +1,106 @@
+#! /bin/sh
+
+ROOT_RUNTIME="/www/tronyes/tronjar"
+DIR_TODO="${ROOT_RUNTIME}/todo"
+
+HOSTNAME="120.77.151.174"
+USER="tronjar"
+
+QuartzTasks="../build/libs/QuartzTasks.jar"
+ServerLauncher="../build/libs/ServerLauncher.jar"
+StartService="start_service.sh"
+
+if [ ! -f "${QuartzTasks}" ]; then
+	echo "${QuartzTasks} 不存在 上线终止"
+	exit
+fi
+
+if [ ! -f "${ServerLauncher}" ]; then
+	echo "${ServerLauncher} 不存在 上线终止"
+	exit
+fi
+
+if [ ! -f "${StartService}" ]; then
+	echo "${StartService} 不存在 上线终止"
+	exit
+fi
+
+read -s -p "输入 $USER@$HOSTNAME 密码"  PASS
+
+# ==========BEGIN==========
+echo "\r清理 $DIR_TODO 文件夹"
+
+TMP_BAK=$(mktemp)
+
+# create expect script
+cat > $TMP_BAK << EOF
+#exp_internal 1 # Uncomment for debug
+set timeout -1
+spawn ssh $USER@$HOSTNAME
+match_max 100000
+expect "*assword:*"
+send -- "$PASS\r"
+expect "Last*"
+send -- "rm -rf $DIR_TODO\r"
+send -- "mkdir $DIR_TODO\r"
+send -- "exit\r"
+interact
+EOF
+
+expect -f $TMP_BAK
+# remove expect script
+rm $TMP_BAK
+# ==========END===========
+
+
+# ==========BEGIN==========
+echo "开始scp文件"
+
+
+TMP_BAK=$(mktemp)
+
+# create expect script
+cat > $TMP_BAK << EOF
+#exp_internal 1 # Uncomment for debug
+set timeout -1
+spawn scp $QuartzTasks $ServerLauncher $StartService $USER@$HOSTNAME:$DIR_TODO
+match_max 100000
+expect "*assword:*"
+send -- "$PASS\r"
+interact
+EOF
+
+expect -f $TMP_BAK
+# remove expect script
+rm $TMP_BAK
+# ==========END===========
+
+# ==========BEGIN==========
+echo "确定要开始执行部署脚本?(y/n)"
+read val
+if [ "$val" != "y" ];  then
+	exit
+fi
+
+TMP_BAK=$(mktemp)
+
+# create expect script
+cat > $TMP_BAK << EOF
+#exp_internal 1 # Uncomment for debug
+set timeout -1
+spawn ssh $USER@$HOSTNAME
+match_max 100000
+expect "*assword:*"
+send -- "$PASS\r"
+expect "Last*"
+send -- "chmod +x $DIR_TODO/$StartService\r"
+send -- "$DIR_TODO/$StartService\r"
+send -- "rm -rf $DIR_TODO/$StartService\r"
+send -- "exit\r"
+interact
+EOF
+
+expect -f $TMP_BAK
+# remove expect script
+rm $TMP_BAK
+# ==========END===========
